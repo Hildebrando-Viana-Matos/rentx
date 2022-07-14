11f7403f124a6f8918b67a1284910522c1d4744f
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-07-14 15:33:06 -0300

    Creating Car component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,11 +4,23 @@ import { StatusBar } from "react-native";
 
 import { RFValue } from "react-native-responsive-fontsize";
 
+import { Car } from "../../components/Car";
+
 import Logo from "../../assets/logo.svg";
 
 import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
+  const carData = {
+    brand: "AUDI",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail:
+      "https://www.audi.ca/content/dam/nemo/ca/Models/rs5/rs5-coupe/MY2020/844x476_rs5coupe-min.png",
+  };
   return (
     <Container>
       <StatusBar
@@ -23,6 +35,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
     </Container>
   );
 }

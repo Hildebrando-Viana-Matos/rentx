d34409d397f81deefcb1c04c3a9d1223d9069f95
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-07-15 15:54:13 -0300

    Details interface

--- src/components/ImageSlider/styles.ts
+++ src/components/ImageSlider/styles.ts
@@ -6,7 +6,7 @@ interface ImageIndexProps {
 }
 
 export const Container = styled.View`
-  flex: 1;
+  width: 100%;
 `;
 
 export const ImageIndexes = styled.View`
@@ -26,7 +26,7 @@ export const ImageIndex = styled.View<ImageIndexProps>`
 `;
 
 export const CarImageWrapper = styled.View`
-  width: ${Dimensions.get("window").width}px;
+  width: ${Dimensions.get('window').width}px;
   height: 132px;
 
   justify-content: center;
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { StatusBar } from "react-native";
+
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 export function CarDetails() {
   return (
     <Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
@@ -24,6 +32,27 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam
+          corporis, dicta, nulla minima harum non, nihil odit quis deserunt
+          laudantium quae exercitationem inventore illum doloribus itaque
+          aperiam quasi temporibus qui.
+        </About>
+      </Content>
     </Container>
   );
 }

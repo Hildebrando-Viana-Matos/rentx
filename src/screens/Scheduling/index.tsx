7921e0f3b7d476731c14a8da985ef60e4316bd43
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-07-16 14:39:25 -0300

    Making Sheduling Interface

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { StatusBar, View } from "react-native";
+
+import { useTheme } from "styled-components";
+
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValueContainer,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+import { Button } from "../../components/Button";
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma{"\n"}
+          data de início e{"\n"}
+          fim do
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue></DateValue>
+            </DateValueContainer>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValueContainer selected={true}>
+              <DateValue>18/07/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
